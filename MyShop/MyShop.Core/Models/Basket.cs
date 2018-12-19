0244966fd276364174e5db8464a62f7989b232d7
ItsYaBoyHorez <rancalucian@yahoo.com>
2018-12-19 12:14:01 +0200

    Added Basket Model

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket :BaseEntity
+    {
+        //by setting it "virtual" EF will know that whenever we try to load the basket from the db it will automatically load all the baskets item as well(LAZY LOADING)
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
