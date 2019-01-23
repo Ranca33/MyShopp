8e54f7fa9d328f365883b623e490f359eb7a7558
ItsYaBoyHorez <rancalucian@yahoo.com>
2019-01-23 12:51:38 +0200

    Added Mocks and Test functionality

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,25 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
 namespace MyShop.WebUI.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class UnitTest1
     {
         [TestMethod]
-        public void Index()
+        public void IndexPageDoesReturnProducts()
         {
-            //// Arrange
-            //HomeController controller = new HomeController();
+            IRepository<Product> productContex = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-            //// Act
-            //ViewResult result = controller.Index() as ViewResult;
+            productContex.Insert(new Product());
+            HomeController controller = new HomeController(productContex, productCategoryContext);
 
-            //// Assert
-            //Assert.IsNotNull(result);
+            var result = controller.Index() as ViewResult;
+            //this is called a cast (ProductListViewModel)
+            //with this we cann acces the various properties of the viewModel
+            var viewModel = (ProductListViewModel)result.ViewData.Model;
+
+            //This test will make sure that there is 1 product in the list of products
+            Assert.AreEqual(1, viewModel.Products.Count());
         }
     }
 }
---
+++ MyShop/MyShop.WebUI.Tests/Mocks/MockContext.cs
@@ -0,0 +1,89 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.WebUI.Tests.Mocks
+{
+    public class MockContext<T> : IRepository<T> where T : BaseEntity
+    {
+        List<T> items;
+        string className;
+
+        public MockContext()
+        {
+            items=new List<T>();
+        }
+
+        //Save the Item in cache
+        public void Commit()
+        {
+            return;
+        }
+
+
+        //Insert Item in List
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        //Check for the item by Id and updates item
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id);
+
+            if (tToUpdate != null)
+            {
+                tToUpdate = t;
+            }
+            else
+            {
+                throw new Exception(className + "Not found");
+            }
+        }
+
+        //Find item in DataBase
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(className + "Not found");
+            }
+        }
+
+        //Returns a list(that can be queried) of items
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+
+        }
+
+        //Check items by Id and deletes it
+        public void Delete(string Id)
+        {
+            T tToDelete = items.Find(i => i.Id == Id);
+
+            if (tToDelete != null)
+            {
+                items.Remove(tToDelete);
+            }
+            else
+            {
+                throw new Exception(className + "Not found");
+            }
+        }
+    }
+}
+
+    
+
